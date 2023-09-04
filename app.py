613755a09e9c0da36c85c603a674cbad182c6853
MishaT <misha_t@ua.fm>
2023-09-05 02:20:55 +0300

    replace application to the real sample

--- app.py
+++ app.py
@@ -1,6 +1,18 @@
-from flask import Flask
+# Uses a single route
+
+from flask import Flask, render_template, request, redirect
+
 app = Flask(__name__)
 
-@app.route('/')
-def hello_world():
-    return 'Hello, Oleksiy!'
+
+@app.route("/", methods=["GET", "POST"])
+def index():
+    if request.method == "POST":
+        return render_template("greet.html", name=request.form.get("name", "world"))
+    return render_template("index.html")
+
+@app.route("/r")
+def r():
+    #return render_template("greet.html", name = "BUG")
+    print("kuku")
+    return redirect("/")
